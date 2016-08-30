0a132b26f152c5994b9c7d8cffd3a721f965c4e6
Kaden Weaver <kpweav13@stlawu.edu>
2016-08-30 09:56:50 -0400

    initial population of files

---
+++ app/src/main/java/com/example/kpweav13/day2activitylifecycle/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.kpweav13.day2activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.hello_view);
+
+        //Anonymous Inner Class
+        tv.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Burnt toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
